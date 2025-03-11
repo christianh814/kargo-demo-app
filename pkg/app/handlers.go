de6cc01faa6d38e85c22ffdef2e16dfb4f5058c6
Christian Hernandez <christian@chernand.io>
2025-03-11 13:36:45 -0700

    updates
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -30,26 +30,3 @@ type Greet struct {
 
 // GreetHtml is the template for the /greet route
 const GreetHtml string = "html/greet.tmpl"
-
-// App configuration file location
-const GreetConfigFile string = "config/greet.json"
-
-/*
-// appGreet is the route that returns the pod information
-func appGreet(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles(GreetHtml))
-	msg, err := utils.LoadConfig(GreetConfigFile)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "Internal Error occurred")
-		return
-	}
-
-	config := Greet{
-		Msg: msg,
-	}
-
-	// Display index page from template
-	tmpl.Execute(w, config)
-}
-*/
