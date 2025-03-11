34ac7bec0b2b7e34f6a086065cc52dac62961ab1
Christian Hernandez <christian@chernand.io>
2025-03-11 14:14:34 -0700

    v1.2.3
    Signed-off-by: Christian Hernandez <christian@chernand.io>

---
+++ pkg/app/handlers.go
@@ -0,0 +1,32 @@
+package app
+
+import (
+	"html/template"
+	"net/http"
+)
+
+// AppConfig type
+type AppConfig struct {
+	Status string
+}
+
+// IndexHtml is the template for the / route
+const IndexHtml string = "html/index.tmpl"
+
+// appRoot is the HTTP root of the application
+func appRoot(w http.ResponseWriter, r *http.Request) {
+	// Parse the config file (it's empty/static for the / route)
+	tmpl := template.Must(template.ParseFiles(IndexHtml))
+	config := AppConfig{}
+
+	// Display index page from template - static for now
+	tmpl.Execute(w, config)
+}
+
+// Greet Struct
+type Greet struct {
+	Msg string
+}
+
+// GreetHtml is the template for the /greet route
+const GreetHtml string = "html/greet.tmpl"
